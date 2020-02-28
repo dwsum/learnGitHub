339fbc3d99d2e59b99b4c044e9d4e852a238cd39
Drew <drewsumsion10@gmail.com>
2020-02-28 14:21:20 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/learnGitHub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnGitHub.dir/main.cpp.o"
+  "learnGitHub"
+  "learnGitHub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnGitHub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "git and push from gitHub" << std::endl;
+    return 0;
+}
